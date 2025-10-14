b57612940ef556c26be96ab55a21c3f28a098342
aradhanahegde <aradhanahegde@ufl.edu>
2025-10-13 20:18:43 -0400

    Created a basic React App

---
+++ cruise-road/src/Header.jsx
@@ -0,0 +1,16 @@
+function Header() {
+    return (
+        <header>    
+            <h1>Cruise Road</h1>
+            <nav>
+                <ul>
+                    <li><a href ="#">Home</a></li>
+                    <li><a href ="#">About</a></li>
+                    <li><a href ="#">Log In/Create an Account</a></li>
+                </ul>
+            </nav>
+            <hr></hr>
+        </header>
+    );
+}
+export default Header;
