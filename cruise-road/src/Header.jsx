46dafd60ba93b4d919065e9f14ae7a09cc12d98b
aradhanahegde <aradhanahegde@ufl.edu>
2025-10-16 16:04:14 -0400

    Modified the homepage

--- cruise-road/src/Header.jsx
+++ cruise-road/src/Header.jsx
@@ -2,11 +2,11 @@ function Header() {
     return (
         <header>    
             <h1>Cruise Road</h1>
+            <h2>Your personal road trip planner</h2>
             <nav>
                 <ul>
-                    <li><a href ="#">Home</a></li>
-                    <li><a href ="#">About</a></li>
-                    <li><a href ="#">Log In/Create an Account</a></li>
+                    <li><a href ="#">Login</a></li>
+                    <li><a href ="#">New? Create an Account</a></li>
                 </ul>
             </nav>
             <hr></hr>
