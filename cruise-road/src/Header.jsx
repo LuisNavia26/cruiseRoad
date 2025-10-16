d936d02cdc8d161e59b87222ddee2e43e6837a52
aradhanahegde <aradhanahegde@ufl.edu>
2025-10-16 19:22:07 -0400

    Added a background image and login buttons

--- cruise-road/src/Header.jsx
+++ cruise-road/src/Header.jsx
@@ -5,10 +5,30 @@ function Header() {
             <h2>Your personal road trip planner</h2>
             <nav>
                 <ul>
-                    <li><a href ="#">Login</a></li>
-                    <li><a href ="#">New? Create an Account</a></li>
+                    <li style={{fontWeight: "bold"}}>LOGIN</li>
+                    <li></li>
                 </ul>
             </nav>
+
+            {/*Login*/}
+
+            <form style={{ marginTop: "20px", display: "inline-block", textAlign: "left" }}> 
+                <div style={{ marginBottom: "10px" }}>
+                    <label htmlFor="username">Username:</label><br />
+                    <input type="text" id="username" name="username" />
+                </div>
+                <div style={{ marginBottom: "10px" }}>
+                    <label htmlFor="password">Password:</label><br />
+                    <input type="password" id="password" name="password" />
+                </div>
+                <button type="submit">Login</button>
+            </form>
+
+            {/*Create an account*/}
+            <div style={{ marginTop: "15px" }}>
+                <a href ="#">New Here? Create an Account</a>
+            </div>
+            
             <hr></hr>
         </header>
     );
